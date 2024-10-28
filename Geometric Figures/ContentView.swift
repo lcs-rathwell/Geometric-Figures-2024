9a2ee787957155e54c0981e7709bfbe95f4d0dde
Jack Rathwell <jrathwell@lcs.on.ca>
2024-10-28 09:22:41 -0400

    Initial Commit

---
+++ Geometric Figures/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Geometric Figures
+//
+//  Created by Jack William Rathwell on 2024-10-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
