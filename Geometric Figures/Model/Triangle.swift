1267fca05fd04f78fe7b0be8919e04ebeada6083
Jack Rathwell <jrathwell@lcs.on.ca>
2024-10-29 11:32:53 -0400

    Added triangle view structure to our model, added basic view to control all dimensions

---
+++ Geometric Figures/Model/Triangle.swift
@@ -0,0 +1,8 @@
+//
+//  Triangle.swift
+//  Geometric Figures
+//
+//  Created by Jack William Rathwell on 2024-10-28.
+//
+
+import Foundation
---
+++ Geometric Figures/views/TriangleView.swift
@@ -0,0 +1,18 @@
+//
+//  TriangleView.swift
+//  Geometric Figures
+//
+//  Created by Jack William Rathwell on 2024-10-28.
+//
+
+import SwiftUI
+
+struct TriangleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TriangleView()
+}
