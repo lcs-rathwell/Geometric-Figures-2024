8031701910a3a7a6d66559f8525f32fd87a70bab
Jack Rathwell <jrathwell@lcs.on.ca>
2024-10-29 11:39:34 -0400

    added additional text views to identify the computed properties

--- Geometric Figures/Model/Triangle.swift
+++ Geometric Figures/Model/Triangle.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+// Define the protocol
+protocol Describable {
+    var description: String { get }
+}
+
+// Define the structure, adopting the new protocol
+struct Triangle: Describable {
+    
+    //PROCESS
+    //MARK: Stored Properties
+    var height: Double
+    var a: Double
+    var b: Double
+    var c: Double
+    
+    //MARK: Computed Properties
+    var perimeter: Double {
+        return a + b + c
+    }
+    
+    var area: Double {
+        return (a+b) * height / 2
+    }
+    
+    var description: String {
+        return "The area of the triangle is \(area) and the perimeter is \(perimeter)"
+    }
+    
+    
+    
+}
--- Geometric Figures/views/TriangleView.swift
+++ Geometric Figures/views/TriangleView.swift
@@ -8,8 +8,59 @@
 import SwiftUI
 
 struct TriangleView: View {
+
+    //MARK: STORED PROPERTIES
+    @State var currentTriangle = Triangle(
+        height: 50 ,
+        a: 50,
+        b: 50,
+        c: 50
+    )
+    //MARK: COMPUTED PROPERTIES
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            // Add an image
+            
+            //Label
+            Text ("Dimensions")
+            Text ("Side A is: \(currentTriangle.a.formatted())")
+            
+            //Sliders
+            //IMPUT
+            Slider(
+                value: $currentTriangle.a,
+                in: 1...100,
+                step: 1.0
+            
+            )
+            Text ("Side B is: \(currentTriangle.b.formatted())")
+            
+            Slider(
+                value: $currentTriangle.b,
+                in: 1...100,
+                step: 1.0
+            )
+            Text ("Side C is : \(currentTriangle.c.formatted())")
+            Slider(
+                    value: $currentTriangle.c,
+                    in: 1...100,
+                    step: 1.0
+                )
+            Text ("Height is: \(currentTriangle.height.formatted())")
+            
+            Slider(
+                value: $currentTriangle.height,
+                    in: 1...100,
+                    step: 1.0
+                )
+            //OUTPUT
+            //Label (Show the Perimiter)
+            Text ("The Perimeter is : \(currentTriangle.perimeter.formatted())")
+            
+            //Label (Show the Area)
+            Text ("The Area is : \(currentTriangle.area.formatted())")
+        }
     }
 }
 
